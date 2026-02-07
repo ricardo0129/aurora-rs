027e91238f205f005881dcadaa369a0fa3121a94
ricardo0129 <ruizr274@gmail.com>
2026-02-06 22:25:27 -0500

    reducing git wf

--- src/communication/transport.rs
+++ src/communication/transport.rs
@@ -13,37 +13,33 @@ pub enum Transport {
 }
 
 pub struct SerialTransport {
-    data_pin: Option<TxDataPin>,
+    data_pin: TxDataPin,
 }
 
 impl SerialTransport {
     pub fn new(data_pin: TxDataPin) -> Self {
-        Self {
-            data_pin: Some(data_pin),
-        }
+        Self { data_pin }
     }
 
     pub fn write_byte(&mut self, mut byte: u8, delay: &mut Delay) {
-        let pin = self.data_pin.as_mut().unwrap();
-        pin.set_low();
+        self.data_pin.set_low();
         delay.delay_us(BIT_US);
         for _ in 0..8 {
             if byte & 1 > 0 {
-                pin.set_high();
+                self.data_pin.set_high();
             } else {
-                pin.set_low();
+                self.data_pin.set_low();
             }
             delay.delay_us(BIT_US);
             byte >>= 1;
         }
-        pin.set_high();
+        self.data_pin.set_high();
         delay.delay_us(BIT_US);
     }
 
     pub fn read_byte(&mut self, delay: &mut Delay) -> u8 {
         let mut data: u8 = 0;
-        let tx_pin: TxDataPin = self.data_pin.take().unwrap();
-        let mut rx_pin = tx_pin.as_input();
+        let mut rx_pin = self.data_pin.as_input();
         while rx_pin.is_high().expect("couldn't read from pin") {}
         delay.delay_us(BIT_US + BIT_US / 2);
         for i in 0..8 {
@@ -53,7 +49,28 @@ impl SerialTransport {
             delay.delay_us(BIT_US);
         }
         delay.delay_us(BIT_US);
-        self.data_pin = Some(tx_pin);
         data
     }
+
+    /*
+    pub fn is_high(&mut self) -> bool {
+        self.data_pin
+            .as_input()
+            .is_high()
+            .expect("couldn't read from pin")
+    }
+
+    fn sync_recv(&mut self, delay: &mut Delay) {
+        self.data_pin.set_low();
+    }
+
+    fn sync_send(&mut self, delay: &mut Delay) {
+        self.data_pin.set_high();
+        while self.is_high() {}
+    }
+
+    pub fn send_transaction(&mut self, delay: &mut Delay) {}
+
+    pub fn receive_transaction(&mut self, delay: &mut Delay) {}
+    */
 }
