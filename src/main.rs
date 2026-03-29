088022ea6fa0afb87ec9a92f02caea6fe9ce54fb
ricardo0129 <ruizr274@gmail.com>
2026-03-28 23:40:30 -0400

    blinky with embassy-rs, its kinda nice

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
-use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
-use nrf52840_hal as hal;
-use nrf52840_hal::gpio::Level;
-use panic_probe as _;
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
 
-use cortex_m_rt::entry;
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    let mut led = Output::new(p.P0_06, Level::Low, OutputDrive::Standard);
 
-#[entry]
-fn main() -> ! {
-    info!("Hello, world!");
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let mut led = port0.p0_17.into_push_pull_output(Level::Low);
     loop {
-        led.set_high().unwrap();
-        defmt::println!("LED on");
-        cortex_m::asm::delay(10_000_000);
-        led.set_low().unwrap();
-        defmt::println!("LED off");
-        cortex_m::asm::delay(10_000_000);
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
     }
 }
